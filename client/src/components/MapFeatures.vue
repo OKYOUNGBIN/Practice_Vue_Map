5e0935bc20955e7df67efaa86125c6f63e46fea8
youngbin <redmangos@naver.com>
2022-05-31 21:47:14 +0900

    add

--- client/src/components/MapFeatures.vue
+++ client/src/components/MapFeatures.vue
@@ -8,9 +8,25 @@
         class="pl-9 pr-4 py-3 text-sm focus:outline-none w-full shadow-md rounded-md"
         type="text"
         placeholder="Start Your Search"
+        v-model="searchQuery"
+        @input="search"
       />
       <!-- search Icon -->
-      <div class="absolute top-0 left-[8px] h-full flex items-center"></div>
+      <div class="absolute top-0 left-[8px] h-full flex items-center">
+        <i class="fas fa-search"></i>
+      </div>
+      <!-- Search Results -->
+      <div class="absolute mt-2 w-full">
+        <!-- Results -->
+        <div class="h-[200px] overflow-scroll bg-white rounded-md">
+          <div
+            class="px-4 py-2 flex gap-x-2 cursor-pointer hover:bg-slate-600 hover:text-white"
+          >
+            <i class="fas fa-map-marker-alt"></i>
+            <p class="text-xs">Testing Result</p>
+          </div>
+        </div>
+      </div>
     </div>
     <!-- Geolocation -->
     <div
@@ -27,7 +43,39 @@
 </template>
 
 <script>
+import ref from "vue";
+import axios from "axios";
+
 export default {
   props: ["coords", "fetchCoords"],
+  setup(props) {
+    const searchQuery = ref(null);
+    const searchData = ref(null);
+    const queryTimeout = ref(null);
+
+    const search = () => {
+      clearTimeout(queryTimeout.value);
+
+      queryTimeout.value = setTimeout(async () => {
+        if (searchQuery.value !== "") {
+          const params = new URLSearchParams({
+            fuzzyMatch: true,
+            language: "en",
+            limit: 10,
+            proximity: props.coords
+              ? `${props.coords.lng}, ${props.coords.lng},${props.coords.lat}`
+              : "0,0",
+          });
+          const getData = await axios.get(
+            `http://localhost:8000/api/search/${searchQuery.value}?${params}`
+          );
+          searchData.value = getData.data.features;
+          console.log(searchData.value);
+        }
+      }, 750);
+    };
+
+    return { searchQuery, searchData, queryTimeout, search };
+  },
 };
 </script>
